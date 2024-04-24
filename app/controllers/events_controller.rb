d2a7eba25b1cf6d9b1f989472a6ed80ce0763786
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-04-24 18:38:54 -0400

    Add create and new action for event with view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,25 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: %i[new create]
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to root_path, notice: 'You\'ve created an event!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :title)
   end
 end
