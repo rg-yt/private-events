635bb5929ad1da2ef8b0f2708b19f9ef3326d94b
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-04-27 19:54:55 -0400

    Update user contoller and view to show events by date

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @events = current_user.created_events
-    @joined_events = current_user.attended_events
+    @user = current_user
   end
 end
