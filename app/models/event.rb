d2db1625eaf949e19c2ef706a0dd6ae3888169d2
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-04-27 20:09:00 -0400

    Add class methods and update views for past/future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,15 @@
 class Event < ApplicationRecord
+  default_scope { order(date: :asc) }
   validates :date, presence: true
   belongs_to :creator, class_name: 'User'
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
+
+  def self.past
+    where('date <= ?', DateTime.current.to_date)
+  end
+
+  def self.future
+    where('date > ?', DateTime.current.to_date)
+  end
 end
