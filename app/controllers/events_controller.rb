45d8130df9d2b490e05c4c205a7938d28dc918c9
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-04-29 12:38:43 -0400

    Update create action to use an instance variable

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,8 +11,8 @@ class EventsController < ApplicationController
   end
 
   def create
-    event = current_user.created_events.build(event_params)
-    if event.save
+    @event = current_user.created_events.build(event_params)
+    if @event.save
       redirect_to root_path, notice: 'You\'ve created an event!'
     else
       render :new, status: :unprocessable_entity
