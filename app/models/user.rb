6c9ec0b274d81a9e45e36fa0d022c5b224f3c1f5
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-04-29 18:01:56 -0400

    Update the user model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :recoverable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
-  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
+  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event', inverse_of: 'creator', dependent: :destroy
   has_many :attendances, dependent: :destroy
   has_many :attended_events, through: :attendances, source: :event
 end
